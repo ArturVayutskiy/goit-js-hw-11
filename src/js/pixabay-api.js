cd85d64c2bdda247c0683aa61092a44f6cb45be1
ArturVayutskiy <vayutskiy@gmail.com>
2024-04-05 19:57:28 +0300

    save

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,1 +1,20 @@
-// Functions for HTTP requests
+export function pixabayAPI(images) {
+    const BASE_URL = 'https://pixabay.com/api/';
+    const API_KEY = '43235509-ebd3145eb150405846e36d932';
+
+    const params = new URLSearchParams({
+        key: API_KEY,
+        q: images,
+        image_type: "photo",
+        orientation: "horizontal",
+        safesearch: "true"
+    })
+
+   return fetch (`${BASE_URL}?${params}`)
+    .then(response => {
+        if(!response.ok){
+            throw new Error(response.statusText)
+        }
+        return response.json()
+    })
+};
